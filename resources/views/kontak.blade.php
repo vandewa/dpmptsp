495366208d1936fbab2b2c17bfb1371af50d8d6b
vandewa <devandewananta@gmail.com>
2024-01-27 21:05:51 +0700

    bug informasi umum kontak kami

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
-                        {{ Form::model($data, ['route' => ['sosmed.post']]) }}
+                        {{ Form::model($data, ['route' => ['kontak.post']]) }}
                         <!-- /.card-header -->
                         <!-- Input addon -->
                         <div class="card card-info mt-3">
